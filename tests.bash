8c7ef7e250d6eeae2b3cd4bfbc2b1368ecfecef6
mkg20001 <mkg20001@gmail.com>
2016-10-23 19:31:43 +0200

    Fix tests

--- tests.bash
+++ tests.bash
@@ -9,21 +9,37 @@ hasfailed=0
 test_fail() {
   hasfailed=1
   echo "[fail] $1"
-  echo "Expected: $2"
-  echo "Got: $3"
+  echo " - Expected: $2"
+  echo " - Got: $3"
+  echo " - $(echo $4 | grep '^ERROR:[a-zA-Z0-9_! -]*' -o)"
+}
+
+test_pass() {
+  echo "[ok] Passed!"
 }
 
 tests() {
   echo
   echo "[test] $1"
-  output=$($prg $2)
-  echo "result:
-$output"
+  output=$($prg $2 2>&1 | sed -e 's#^[0-9]*:[0-9]*:[0-9]* ##')
   ex=$?
-  pass=1
-  if [ ! $ex -eq $3 ]; then test_fail ""
-  if [ ! -z $4 ]; if [ "$output" != "$4" ]; then test_fail "Wrong output" $4 $output
+  echo "$output"
+  if [ ! $ex -eq $3 ]; then test_fail "Wrong exit code" $3 $ex "$output"; return 2; fi
+  if [ ! -z "$4" ]; then
+    if [ "$output" != "$4" ]; then test_fail "Wrong output" "$4" "$output" "$output"; return 2; fi
+  fi
+  test_pass
 }
+
+tests "Update Cache" "update-cache" 0 "Fetching version lists...
+fs-repo-migrations
+go-ipfs
+gx
+gx-go
+ipfs-see-all
+ipfs-update
+ipget
+Done!"
 
 for soft in "${list[@]}"; do
   tests "Install $soft" "install $soft" 0
